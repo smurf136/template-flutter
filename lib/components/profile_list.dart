0a19bc68773130a9e50f8391634a8c33fe29876e
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-08-18 17:56:13 +0700

    [Add] create component list in profile page

---
+++ lib/components/profile_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+Widget profile_list(BuildContext context,{
+  @required IconData icon,
+  @required String title,
+  @required bool isLast,
+  Widget widget,
+}){
+  return Column(
+    children: [
+      Padding(
+        padding: EdgeInsets.all(30),
+        child: Container(
+          child: Row(
+            children: [
+              Icon(icon),
+              Padding(padding: EdgeInsets.all(10),),
+              Text(title)
+            ],
+          )
+        ),
+      ),
+      widget != null ?
+      widget : Container(),
+      isLast ?
+      Container() :
+      Divider(
+        thickness: 3,
+        indent: 75,
+        endIndent: 55,
+      ),
+    ],
+  );
+}
